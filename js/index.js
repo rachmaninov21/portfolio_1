284c9e43071586329702e80be04c9380b2f47f7a
tsuge <mama>
2021-06-26 02:08:55 +0900

    jsとcss

---
+++ js/index.js
@@ -0,0 +1,9 @@
+//function clickTest() {
+//	alert("wawawawawa");
+//}
+
+var top = document.getElementById('btn_top_id');
+var text = document.getElementById('text');
+
+
+top.addEventListener('click', event => { text.innerHTML = '‚¨‚ñ‚ ‚“‚¾' });
