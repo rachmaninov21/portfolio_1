ab3ca15341e0e1ae255dc3885f20808645214ae2
tsuge <mama>
2021-06-26 12:57:55 +0900

    no message

--- js/index.js
+++ js/index.js
@@ -5,10 +5,5 @@ $('.slider').slick();
 var top = document.getElementsByClassName('btn_top');
 var text = document.getElementById('text');
 
-$(function () {
-	//共通ヘッダー
-	$("header").load('header.html');
-	//共通フッター
-	$("footer").load('footer.html');
-});
+
 //top.addEventListener('click', event => { text.innerHTML = 'おんあｓだ' });
