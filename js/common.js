5cbc6ddc521b35d5b5ccf3570fbd45c04ddae32f
tsuge <mama>
2021-06-26 11:48:08 +0900

    フッターとヘッダーを別ファイル化

---
+++ js/common.js
@@ -0,0 +1,6 @@
+$(function () {
+	//共通ヘッダー
+	$("header").load('header.html');
+	//共通フッター
+	$("footer").load('footer.html');
+});
--- js/index.js
+++ js/index.js
@@ -5,5 +5,10 @@ $('.slider').slick();
 var top = document.getElementsByClassName('btn_top');
 var text = document.getElementById('text');
 
-
-top.addEventListener('click', event => { text.innerHTML = 'おんあｓだ' });
+$(function () {
+	//共通ヘッダー
+	$("header").load('header.html');
+	//共通フッター
+	$("footer").load('footer.html');
+});
+//top.addEventListener('click', event => { text.innerHTML = 'おんあｓだ' });
